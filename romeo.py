7c13719da796a0d5d27fa80b9b41a98b67b92369
Abhiraj Mengade <i2am@yandex.com>
2021-11-17 11:04:03 +0530

    First command
    Added a command of Hello World.

--- romeo.py
+++ romeo.py
@@ -1,6 +1,7 @@
 import os
 import discord
 from dotenv import load_dotenv
+import random
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -10,5 +11,17 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  msg = message.content
+
+  if msg.startswith("/hello"):
+    await message.channel.send("Hello! I am Romeo :)")
+    await message.channel.send('https://picsum.photos/'+ str(random.randint(500,1000)))
 
 client.run(TOKEN)
