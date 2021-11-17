09e9b4eeb49c730a3195b432a9a4558cb26d24d0
Abhiraj Mengade <i2am@yandex.com>
2021-11-17 21:46:56 +0530

    Commit 4. Addition of Readme, License and other minor things.
    Modified defn and dict function, Building of Readme & lic.

---
+++ README.md
@@ -0,0 +1,70 @@
+## *Romeo Bot 👑*
+-*A discord bot with dictionary functionalities.*
+-*Use this link to invite it to your server :) shorturl.at/bvR27*
+-*Built as a task for WEC Recruitment 2021*
+
+***Built using discord.py and dictionary api, this bot provides multiple commands to use upon words such as:***
+
+- r.help - a generic help command
+
+- r.dict <word> - Displays definition, phonetic, Origin, Synonyms, Antonyms and other info about the word.
+
+- r.defn <word> - Displays definition, phonetic and other info about the word.
+
+- r.syn <word> - Displays synonyms of the word
+
+- r.anty <word> - Displays antonyms of the word
+
+- r.org <word> - Displays Origin of the word
+
+- r.ana <word> - Displays a list of Anagrams of the word
+
+- r.quote - Displays a curious random quote & an image!
+
+## *Romeo Bot 👑*
+-*A discord bot with dictionary functionalities.*
+-*Use this link to invite it to your server :) shorturl.at/bvR27*
+-*Built as a task for WEC Recruitment 2021*
+
+***Built using discord.py and dictionary api, this bot provides multiple commands such as:***
+
+### Commands:
+- r.help - *a generic help command*
+
+- r.dict <word> - *Displays definition, phonetic, Origin, Synonyms, Antonyms and other info about the word.*
+
+- r.defn <word> - *Displays definition, phonetic and other info about the word.*
+
+- r.syn <word> - *Displays synonyms of the word*
+
+- r.anty <word> - *Displays antonyms of the word*
+
+- r.org <word> - *Displays Origin of the word*
+
+- r.ana <word> - *Displays a list of Anagrams of the word*
+
+- r.quote - *Displays a curious random quote & an image!*
+- 
+### Screenshots
+![r.help](/images/help.jpeg "r.help")
+![r.dict <word>](/images/dict.jpeg "r.dict")
+![r.defn <word>](/images/defn.jpeg "r.defn")
+![r.syn <word>](/images/syn.jpeg "r.syn")
+![r.anty <word>](/images/anty.jpeg "r.anty")
+![r.org <word>](/images/org.jpeg "r.org")
+![r.ana <word>](/images/ana.jpeg "r.ana")
+![r.quote](/images/ana.jpeg "r.quote")
+
+
+***Flask is used to start a running server.***
+
+
+
+
+
+
+***Flask is used to start a running server.***
+
+
+
+
--- romeo.py
+++ romeo.py
@@ -1,3 +1,4 @@
+#imports
 import os
 import discord
 from dotenv import load_dotenv
@@ -6,18 +7,26 @@ import requests
 import random
 from keep_alive import keep_alive
 
+#Setting the Client & Loading the TOKEN
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
 client = discord.Client()
 
+"""
+get_quote():
+Gets a random quote from the zenquotes api and returns back a string.
+"""
 def get_quote():
   response = requests.get("https://zenquotes.io/api/random")
   json_data = json.loads(response.text)
   quote = json_data[0]["q"] + " -" + json_data[0]["a"]
   return(quote)
 
-
+"""
+get_dict(s):
+Takes input of a string and request data from the dictionary api.
+Returns a string including, Part of Speech, Definition, Phonetic, origin and Example of the word.
+"""
 def get_dict(s):
   response = requests.get("https://api.dictionaryapi.dev/api/v2/entries/en/"+s)
   json_data = json.loads(response.text)
@@ -27,10 +36,42 @@ def get_dict(s):
   defn = json_data[0]["meanings"][0]["definitions"][0]["definition"]
   phonetic = json_data[0]["phonetic"]
   example = json_data[0]["meanings"][0]["definitions"][0]["example"]
-  s = "**"+s.upper()+"**\n\n"+"**Part Of Speech:**"+" *"+pos+"*\n"+"**Defn:**"+" *"+defn+"*\n"+"**Phonetic:**"+" *"+phonetic+"*\n"+"**Example:**"+" *"+example+"*\n"
-
+  org="**No Origin!**"
+  if("origin" in json_data[0]):
+      org = json_data[0]["origin"]
+  s = "**"+s.upper()+"**\n\n"+"**Part Of Speech:**"+" *"+pos+"*\n"+"**Defn:**"+" *"+defn+"*\n"+"**Phonetic:**"+" *"+phonetic+"*\n"+"**Example:**"+" *"+example+"*\n""**Origin:**"+" *"+org+"*\n"
+  syn = json_data[0]["meanings"][0]["definitions"][0]["synonyms"]
+  anty = json_data[0]["meanings"][0]["definitions"][0]["antonyms"]
+  s+="**Synonyms:**\n"
+  for i in range(min(len(syn),3)):
+      s += "*"+syn[i]+"*\n"
+  s+="**Antonyms:**\n"
+  for i in range(min(len(anty),3)):
+      s += "*"+anty[i]+"*\n"
   return(s)
 
+
+"""
+get_defn(s):
+Takes input of a string and request data from the dictionary api.
+Returns a string with definition and phonetic of the word.
+"""
+def get_defn(s):
+  response = requests.get("https://api.dictionaryapi.dev/api/v2/entries/en/"+s)
+  json_data = json.loads(response.text)
+  if(type(json_data)==dict):
+      return "**Wrong Word da!**"
+  pos = json_data[0]["meanings"][0]["partOfSpeech"]
+  phonetic = json_data[0]["phonetic"]
+  defn = json_data[0]["meanings"][0]["definitions"][0]["definition"]
+  s = "**"+s.upper()+"**\n\n"+"**Part Of Speech:**"+" *"+pos+"*\n"+"**Defn:**"+" *"+defn+"*\n"+"**Phonetic:**"+" *"+phonetic+"*\n"
+  return s
+
+"""
+get_syn(s):
+Takes input of a string and request data from the dictionary api.
+Returns a string with synonyms of the word.
+"""
 def get_syn(s):
   response = requests.get("https://api.dictionaryapi.dev/api/v2/entries/en/"+s)
   json_data = json.loads(response.text)
@@ -42,6 +83,11 @@ def get_syn(s):
       s += "***"+syn[i]+"***\n"
   return s
 
+"""
+get_anty(s):
+Takes input of a string and request data from the dictionary api.
+Returns a string with antonyms of the word.
+"""
 def get_anty(s):
   response = requests.get("https://api.dictionaryapi.dev/api/v2/entries/en/"+s)
   json_data = json.loads(response.text)
@@ -53,6 +99,12 @@ def get_anty(s):
       s += "***"+anty[i]+"***\n"
   return s
 
+
+"""
+get_origin(s):
+Takes input of a string and request data from the dictionary api.
+Returns a string with the origin of the word.
+"""
 def get_org(s):
   response = requests.get("https://api.dictionaryapi.dev/api/v2/entries/en/"+s)
   json_data = json.loads(response.text)
@@ -63,6 +115,11 @@ def get_org(s):
       org = json_data[0]["origin"]
   return org
 
+"""
+get_ana(s):
+Takes input of a string and request data from the dictionary api.
+Returns a string with anagrams of the word.
+"""
 def get_ana(s):
   if(len(s))>9:
        return "**Enter a word which has less than 10 characters!**"
@@ -74,13 +131,15 @@ def get_ana(s):
       s += "***"+ana[i]+"***\n"
   return s
 
-
+#message displayed by help text
 help = """
 ***🤖 ROMEO's HELP MENU 🤖 *** 
 
 **r.help - You know what this does!**
 
-**r.dict <word> - Displays definition, phonetic and Type of the word**
+**r.dict <word> - Displays definition, phonetic, Type, origin, synonyms & antonyms of the word**
+
+**r.defn <word> - Displays definition, phonetic and Type of the word**
 
 **r.syn <word> - Displays synonyms of the word**
 
@@ -91,26 +150,31 @@ help = """
 **r.ana <word> - Displays a list of Anagrams of the word**
 
 **r.quote - Displays a curious random quote & an image!**
+
+**Made by - Abhiraj Mengade :)**
 """
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
-
 @client.event
 async def on_message(message):
   if message.author == client.user:
     return
 
+  #storing the user message
   msg = message.content
+
   if msg.startswith("r.help"):
       embedVar = discord.Embed(title="Romeo 👑", description=help, color=0x00ff00)
       await message.channel.send(embed=embedVar)
 
+
   if msg.startswith("r.hello"):
     embedVar = discord.Embed(title="Romeo 👑", description="*Ahoy! Nice to see ya mate* :)", color=0x00ff00)
     await message.channel.send(embed=embedVar)
+
 
   if msg.startswith("r.quote"):
     quote = "***"+get_quote()+"***"
@@ -118,15 +182,29 @@ async def on_message(message):
     await message.channel.send(embed=embedVar)
     await message.channel.send('https://picsum.photos/'+ str(random.randint(500,1000)))
 
+
   if msg.startswith("r.dict"):
       l = msg.split()
       if(len(l)==1):
           file="**Enter the word da!**"
       else:
           file = get_dict(l[1])
-
       embedVar = discord.Embed(title="Dictionary 📘", description=file, color=0x00ff00)
       await message.channel.send(embed=embedVar)
+
+
+  
+  if msg.startswith("r.defn"):
+      l = msg.split()
+      if(len(l)==1):
+          file="**Enter the word da!**"
+      else:
+          file = get_defn(l[1])
+      embedVar = discord.Embed(title="Definition 🖊", description=file, color=0x00ff00)
+      await message.channel.send(embed=embedVar)
+  
+  
+
 
   if msg.startswith("r.syn"):
       l = msg.split()
@@ -134,9 +212,9 @@ async def on_message(message):
           file="**Enter the word da!**"
       else:
           file = get_syn(l[1])
-
       embedVar = discord.Embed(title="Synonym 🖇", description=file, color=0x00ff00)
       await message.channel.send(embed=embedVar)
+ 
 
   if msg.startswith("r.anty"):
       l = msg.split()
@@ -144,9 +222,9 @@ async def on_message(message):
           file="**Enter the word da!**"
       else:
           file = get_anty(l[1])
-
       embedVar = discord.Embed(title="Antonyms 🛠", description=file, color=0x00ff00)
       await message.channel.send(embed=embedVar)
+
 
   if msg.startswith("r.org"):
       l = msg.split()
@@ -154,9 +232,9 @@ async def on_message(message):
           file="**Enter the word da!**"
       else:
           file = get_org(l[1])
-
       embedVar = discord.Embed(title="Origin 🌾", description=file, color=0x00ff00)
       await message.channel.send(embed=embedVar)
+
 
   if msg.startswith("r.ana"):
       l = msg.split()
@@ -164,5 +242,8 @@ async def on_message(message):
       embedVar = discord.Embed(title="Anagram 🧣", description=file, color=0x00ff00)
       await message.channel.send(embed=embedVar)
 
+
+#flask server function
 keep_alive()
+#client run function
 client.run(TOKEN)
